f426d69332eb3c82a26ffffecb69cce9dc7ac992
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2025-01-09 19:54:37 +0100

    feat: add option to use config file

--- cmd/genCert.go
+++ cmd/genCert.go
@@ -5,50 +5,83 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/hm-edu/harica/client"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 type GenCertConfig struct {
-	domains           []string
-	csr               string
-	transactionType   string
-	requesterEmail    string
-	requesterPassword string
-	requesterTOTPSeed string
-	validatorEmail    string
-	validatorPassword string
-	validatorTOTPSeed string
+	Domains           []string `mapstructure:"domains" validate:"required"`
+	Csr               string   `mapstructure:"csr" validate:"required"`
+	TransactionType   string   `mapstructure:"transaction_type" validate:"required"`
+	RequesterEmail    string   `mapstructure:"requester_email" validate:"required"`
+	RequesterPassword string   `mapstructure:"requester_password" validate:"required"`
+	RequesterTOTPSeed string   `mapstructure:"requester_totp_seed"`
+	ValidatorEmail    string   `mapstructure:"validator_email" validate:"required"`
+	ValidatorPassword string   `mapstructure:"validator_password" validate:"required"`
+	ValidatorTOTPSeed string   `mapstructure:"validator_totp_seed" validate:"required"`
 }
 
 var (
 	genCertConfig GenCertConfig
+	configPath    string
 )
 
 // genCertCmd represents the genCert command
 var genCertCmd = &cobra.Command{
 	Use: "gen-cert",
 	Run: func(cmd *cobra.Command, args []string) {
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("cert-generator")
+		viper.AddConfigPath("/etc/harica/")  // path to look for the config file in
+		viper.AddConfigPath("$HOME/harica/") // call multiple times to add many search paths
+		viper.AddConfigPath("/opt/harica/")
+		viper.AddConfigPath(".") // optionally look for config in the working directory
+		if configPath != "" {
+			viper.SetConfigFile(configPath)
+		}
+		if err := viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				slog.Info("No configuration file found")
+			} else {
+				slog.Error("Error reading config file", slog.Any("error", err))
+				os.Exit(1)
+			}
+		}
 
-		requester, err := client.NewClient(genCertConfig.requesterEmail, genCertConfig.requesterPassword, genCertConfig.requesterTOTPSeed, client.WithDebug(debug))
+		viper.BindPFlags(cmd.Flags())
+
+		// Unmarshal the config into a struct.
+		err := viper.Unmarshal(&genCertConfig)
+		if err != nil {
+			slog.Error("Error reading config file", slog.Any("error", err))
+			os.Exit(1)
+		}
+		validate := validator.New()
+		if err := validate.Struct(&genCertConfig); err != nil {
+			slog.Error(fmt.Sprintf("Missing required attributes: \n%v\n", err))
+			os.Exit(1)
+		}
+		requester, err := client.NewClient(genCertConfig.RequesterEmail, genCertConfig.RequesterPassword, genCertConfig.RequesterTOTPSeed, client.WithDebug(debug))
 		if err != nil {
 			slog.Error("failed to create requester client", slog.Any("error", err))
 			os.Exit(1)
 		}
-		validator, err := client.NewClient(genCertConfig.validatorEmail, genCertConfig.validatorPassword, genCertConfig.validatorTOTPSeed, client.WithDebug(debug))
+		validator, err := client.NewClient(genCertConfig.ValidatorEmail, genCertConfig.ValidatorPassword, genCertConfig.ValidatorTOTPSeed, client.WithDebug(debug))
 		if err != nil {
 			slog.Error("failed to create validator client", slog.Any("error", err))
 			os.Exit(1)
 		}
 
-		orgs, err := requester.CheckMatchingOrganization(genCertConfig.domains)
+		orgs, err := requester.CheckMatchingOrganization(genCertConfig.Domains)
 		if err != nil || len(orgs) == 0 {
 			slog.Error("failed to check matching organization", slog.Any("error", err))
 			os.Exit(1)
 		}
 		slog.Info("matching organizations", slog.Any("organizations", orgs))
 
-		transaction, err := requester.RequestCertificate(genCertConfig.domains, genCertConfig.csr, genCertConfig.transactionType, orgs[0])
+		transaction, err := requester.RequestCertificate(genCertConfig.Domains, genCertConfig.Csr, genCertConfig.TransactionType, orgs[0])
 		if err != nil {
 			slog.Error("failed to request certificate", slog.Any("error", err))
 			os.Exit(1)
@@ -83,22 +116,25 @@ var genCertCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(genCertCmd)
-	genCertCmd.Flags().StringSliceVar(&genCertConfig.domains, "domains", []string{}, "Domains to request certificate for")
-	genCertCmd.Flags().StringVar(&genCertConfig.csr, "csr", "", "CSR to request certificate with")
-	genCertCmd.Flags().StringVarP(&genCertConfig.transactionType, "transaction-type", "t", "DV", "Transaction type to request certificate with")
-	genCertCmd.Flags().StringVar(&genCertConfig.requesterEmail, "requester-email", "", "Email of requester")
-	genCertCmd.Flags().StringVar(&genCertConfig.requesterPassword, "requester-password", "", "Password of requester")
-	genCertCmd.Flags().StringVar(&genCertConfig.requesterTOTPSeed, "requester-totp-seed", "", "TOTP seed of requester")
-	genCertCmd.Flags().StringVar(&genCertConfig.validatorEmail, "validator-email", "", "Email of validator")
-	genCertCmd.Flags().StringVar(&genCertConfig.validatorPassword, "validator-password", "", "Password of validator")
-	genCertCmd.Flags().StringVar(&genCertConfig.validatorTOTPSeed, "validator-totp-seed", "", "TOTP seed of validator")
+	genCertCmd.PersistentFlags().StringSlice("domains", []string{}, "Domains to request certificate for")
+	genCertCmd.PersistentFlags().String("csr", "", "CSR to request certificate with")
+	genCertCmd.PersistentFlags().StringP("transaction-type", "t", "DV", "Transaction type to request certificate with")
+	genCertCmd.PersistentFlags().String("requester-email", "", "Email of requester")
+	genCertCmd.PersistentFlags().String("requester-password", "", "Password of requester")
+	genCertCmd.PersistentFlags().String("requester-totp-seed", "", "TOTP seed of requester")
+	genCertCmd.PersistentFlags().String("validator-email", "", "Email of validator")
+	genCertCmd.PersistentFlags().String("validator-password", "", "Password of validator")
+	genCertCmd.PersistentFlags().String("validator-totp-seed", "", "TOTP seed of validator")
 
-	genCertCmd.MarkFlagRequired("domains")             //nolint:errcheck
-	genCertCmd.MarkFlagRequired("csr")                 //nolint:errcheck
-	genCertCmd.MarkFlagRequired("requester-email")     //nolint:errcheck
-	genCertCmd.MarkFlagRequired("requester-password")  //nolint:errcheck
-	genCertCmd.MarkFlagRequired("requester-totp-seed") //nolint:errcheck
-	genCertCmd.MarkFlagRequired("validator-email")     //nolint:errcheck
-	genCertCmd.MarkFlagRequired("validator-password")  //nolint:errcheck
-	genCertCmd.MarkFlagRequired("validator-totp-seed") //nolint:errcheck
+	viper.BindPFlag("domains", genCertCmd.PersistentFlags().Lookup("domains"))
+	viper.BindPFlag("csr", genCertCmd.PersistentFlags().Lookup("csr"))
+	viper.BindPFlag("transaction_type", genCertCmd.PersistentFlags().Lookup("transaction-type"))
+	viper.BindPFlag("requester_email", genCertCmd.PersistentFlags().Lookup("requester-email"))
+	viper.BindPFlag("requester_password", genCertCmd.PersistentFlags().Lookup("requester-password"))
+	viper.BindPFlag("requester_totp_seed", genCertCmd.PersistentFlags().Lookup("requester-totp-seed"))
+	viper.BindPFlag("validator_email", genCertCmd.PersistentFlags().Lookup("validator-email"))
+	viper.BindPFlag("validator_password", genCertCmd.PersistentFlags().Lookup("validator-password"))
+	viper.BindPFlag("validator_totp_seed", genCertCmd.PersistentFlags().Lookup("validator-totp-seed"))
+
+	genCertCmd.PersistentFlags().StringVar(&configPath, "config", "", "config file (default is $HOME/harica/cert-generator.yaml)")
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.23.3
 require (
 	github.com/emersion/go-imap/v2 v2.0.0-beta.4
 	github.com/emersion/go-message v0.18.1
+	github.com/go-playground/validator/v10 v10.23.0
 	github.com/go-resty/resty/v2 v2.16.3
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/miekg/dns v1.1.62
 	github.com/pquerna/otp v1.4.0
 	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.19.0
 	golang.org/x/net v0.34.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -17,13 +19,33 @@ require (
 require (
 	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
 	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/mod v0.18.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )
